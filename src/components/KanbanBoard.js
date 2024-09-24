b23a4eb12314a37b53b049d9caeac5695b7db865
Shreya-2415 <127885027+Shreya-2415@users.noreply.github.com>
2024-09-24 08:16:45 +0530

    KanbanBoard.js

--- src/components/KanbanBoard.js
+++ src/components/KanbanBoard.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import Column from './Column';
-// import { groupTickets } from '../utils/ticketUtils';
-// import './KanbanBoard.css';
-// // import { useViewState } from '../utils/utils.js';
-// // import { FaChevronDown, FaPlus, FaEllipsisH, FaCircle, FaExclamationCircle } from 'react-icons/fa';
-
-// function KanbanBoard({ tickets, users, groupBy, sortOrder }) {
-//   const groupedTickets = groupTickets(tickets, users, groupBy, sortOrder);
-
-//   return (
-//     <div className="kanban-board">
-//       {Object.entries(groupedTickets).map(([group, groupTickets]) => (
-//         <Column key={group} title={group} tickets={groupTickets} users={users} />
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default KanbanBoard;
-
-
-// KanbanBoard.js
 import React from 'react';
 import Column from './Column';
 import { groupTickets, getStatusIcon } from '../utils/ticketUtils';
